2687ff9c089db46d7488419cab8d5795e4bd746a
Arthur Durand <arthurdurand.pro1@gmail.com>
2023-11-09 20:43:29 -0500

    Add documentation and fix interact range

--- Generic_Interactive_System/Source/Generic_Interactive_System/Private/Components/InteractiveComponent.cpp
+++ Generic_Interactive_System/Source/Generic_Interactive_System/Private/Components/InteractiveComponent.cpp
@@ -7,11 +7,13 @@
 #include "Kismet/GameplayStatics.h"
 #include "Net/UnrealNetwork.h"
 
+// Sets default values for this component's properties
 UInteractiveComponent::UInteractiveComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
 }
 
+// Called when the game starts
 void UInteractiveComponent::BeginPlay()
 {
 	Super::BeginPlay();
@@ -27,17 +29,26 @@ void UInteractiveComponent::BeginPlay()
 	}
 }
 
+// Called when the game ends
+void UInteractiveComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+	GetWorld()->GetTimerManager().ClearTimer(ResearchTimerHandle);
+}
+
 void UInteractiveComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	DOREPLIFETIME_CONDITION_NOTIFY(UInteractiveComponent, BestInteractible, COND_OwnerOnly, REPNOTIFY_Always);
 }
 
+// Primary interaction called by the player
 void UInteractiveComponent::PrimaryInteract()
 {
 	ServerPrimaryInteract();
 }
 
+// Server side primary interaction
 void UInteractiveComponent::ServerPrimaryInteract_Implementation()
 {
 	if (BestInteractible != nullptr && BestInteractible->Implements<UInteractiveInterface>())
@@ -46,37 +57,44 @@ void UInteractiveComponent::ServerPrimaryInteract_Implementation()
 	}
 }
 
+// Server side primary interaction validation
 bool UInteractiveComponent::ServerPrimaryInteract_Validate()
 {
-	if (ensureAlways(Owner) && Owner->HasAuthority())
+	// Check if the owner is valid and has authority and if the player is close enough to the interactible
+	if (ensureAlways(Owner) && Owner->HasAuthority() && BestInteractible->GetDistanceTo(Owner) < 100.f)
 	{
 		return true;
 	}
 	return false;
 }
 
+// Secondary interaction called by the player
 void UInteractiveComponent::SecondaryInteract()
 {
 	ServerSecondaryInteract();
 }
 
+// Server side secondary interaction
 void UInteractiveComponent::ServerSecondaryInteract_Implementation()
 {
+	// Check if the owner is valid and has authority and if the player is close enough to the interactible
 	if (BestInteractible != nullptr && BestInteractible->Implements<UInteractiveInterface>())
 	{
 		Cast<IInteractiveInterface>(BestInteractible)->OnSecondaryInteract(Owner);
 	}
 }
 
+// Server side secondary interaction validation
 bool UInteractiveComponent::ServerSecondaryInteract_Validate()
 {
-	if (ensureAlways(Owner) && Owner->HasAuthority())
+	if (ensureAlways(Owner) && Owner->HasAuthority()  && BestInteractible->GetDistanceTo(Owner) < 100.f)
 	{
 		return true;
 	}
 	return false;
 }
 
+// Get all the actors that implement the IInteractiveInterface
 void UInteractiveComponent::GetAllInteractible()
 {
 	UGameplayStatics::GetAllActorsWithInterface(GetWorld(), UInteractiveInterface::StaticClass(), ListOfInteractibles);
@@ -94,6 +112,7 @@ void UInteractiveComponent::GetAllInteractible()
 	}
 }
 
+// Try to set the new best interactible by comparing the distance between the current best interactible and the new one
 void UInteractiveComponent::TrySetNewBestInteractible(AActor* RelevantActor)
 {
 	if (BestInteractible != RelevantActor)
